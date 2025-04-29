a98b1d03149bdb087a68fcfb14e8dad69553709b
Carlos Garcia <mrgarcia7@gmail.com>
2025-04-29 12:30:34 -0500

    implement button to switch between themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
+"use client"
+
+import { useTheme } from "next-themes";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="bg-amber-900 dark:bg-blue-800 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -49,6 +64,12 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <button
+            className="border border-blue-700 dark:border-white p-3 rounded-full cursor-pointer"
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          >
+            Switch theme
+          </button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
@@ -98,6 +119,6 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
-    </div>
+    </div >
   );
 }
