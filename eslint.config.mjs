03938b807563c023152a9f5fa783ff422349c763
Carlos Garcia <mrgarcia7@gmail.com>
2025-05-05 21:16:34 -0500

    set up prettier rules and rules for Tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,37 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      "plugin:prettier/recommended",
+    ],
+    plugins: ["prettier", "tailwindcss"],
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          trailingComma: "all",
+          semi: false,
+          tabWidth: 2,
+          printWidth: 80,
+          endOfLine: "auto",
+          arrowParens: "always",
+        },
+      ],
+      "tailwindcss/classnames-order": "warn",
+    },
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig
